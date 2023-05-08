8bf45cbbb0df86ec3d3993594b84dd5e788a3813
NohanCS <nohansilveira@gmail.com>
2023-05-08 12:29:28 -0300

    loja de Produtos

---
+++ Configuracao.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAplication1
+{
+    class Configuracao
+    {
+        public int numero { get; set; }
+        
+        public Configuracao() { }
+
+        public void Configuracao1()
+        {
+            Console.WriteLine("- Computador Completo Requisitos -");
+            Console.WriteLine("Monitor: 49 polegadas 144hz");
+            Console.WriteLine("Gabinete: Atx");
+            Console.WriteLine("Placa Mãe: Z650m DDR5");
+            Console.WriteLine("Processador: Ryzen 7 7800x");
+            Console.WriteLine("Memoria: Corsair DDR5 6800mhz");
+            Console.WriteLine("Placa de Vídeo: RTX 4070 ti");
+            Console.WriteLine("Fonte: 850w plus Bronze");
+            Console.WriteLine("2x SSDS nvme 2.0 1tb, nvme 2.0 250gb");
+            Console.WriteLine("teclado: Mecânico com rgb");
+            Console.WriteLine("Mouse Pad: 100x200x");
+            Console.WriteLine("Mouse: 8000pi rgb");
+            Console.WriteLine("");
+
+            Console.Write("Você deseja completar essa compra?  ");
+            char compra = char.Parse(Console.ReadLine());
+            if (compra == 's' || compra == 'S')
+            {
+                Console.WriteLine("Compra efetuada com sucesso! ");
+                Console.WriteLine("Agradeçemos a sua compra volte sempre! :)");
+            }
+            else
+            {
+                Console.WriteLine("Compra não efetuada com sucesso!  ");
+                Console.WriteLine();                
+            }
+        }
+
+        public void Configuracao2()
+        {
+            Console.WriteLine("- Notebook Requisitor -");
+            Console.WriteLine("Memória Ram: 16gb (2x8) 6800mhz DDR5");
+            Console.WriteLine("Processador: i7 11800H");
+            Console.WriteLine("SSD: nvme 2.0 512gb");
+            Console.WriteLine("SO: Windows 11 Pro");
+            Console.WriteLine("Placa Gráfica: RTX 3070 4gb");
+            Console.WriteLine(" ");
+
+            Console.Write("Você deseja completar essa compra? ");
+            char compra = char.Parse(Console.ReadLine());
+            if (compra == 's' || compra == 'S')
+            {
+                Console.WriteLine("Compra efetuada com sucesso! ");
+                Console.WriteLine("Agradeçemos a sua compra volte sempre! :)");
+            }
+            else
+            {
+                Console.WriteLine("Compra não efetuada com sucesso! ");
+                Console.WriteLine("");                
+            }
+        }
+
+        public void Configuracao3()
+        {
+            Console.WriteLine(" - Celular Asus Rog Phone 5 - ");
+            Console.WriteLine("Placa Gráfica: SnapDragon 888");
+            Console.WriteLine("Memória ram: 12gb");
+            Console.WriteLine("Memória Interna: 256gb");
+            Console.WriteLine("Duração Báteria: 6.000 mAh");
+            Console.WriteLine("Câmera Frontal: 50mg pixel");
+            Console.WriteLine("Câmera Traseira: 50mg pixel");
+            Console.WriteLine("Dimensão da tela: 1080x2448 Pixels");
+            Console.WriteLine("frequencia tela: 144mhz");
+            Console.WriteLine(" ");
+
+            Console.Write("Você deseja completar essa compra? ");
+            char compra = char.Parse(Console.ReadLine());
+            if (compra == 's' || compra == 'S')
+            {
+                Console.WriteLine("Compra efetuada com sucesso! ");
+                Console.WriteLine("Agradeçemos a sua compra volte sempre! :)");
+            }
+            else
+            {
+                Console.WriteLine("Compra não efetuada com sucesso! ");
+                Console.WriteLine(" ");               
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleAplication1
+{
+    class Program
+    {
+        static void Main( string [] args )
+        {
+            Configuracao conf = new Configuracao();
+
+            Console.WriteLine("Escolha um item da loja.");
+            Console.WriteLine(" ");
+            Console.WriteLine("----- Tabela de Preços -----");
+            Console.WriteLine($"{1}: Computador Completo - R$ 14.900");
+            Console.WriteLine($"{2}: Notebook Gamer - R$8.000");
+            Console.WriteLine($"{3}: Celular Asus Rog Phone - R$ 5.000");
+            Console.WriteLine(" ");
+
+            Console.Write("Digite um número da sua compra! ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero > 0 && numero < 2)
+            {
+                conf.Configuracao1();
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            if (numero > 1 && numero < 3)
+            {
+                conf.Configuracao2();
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            if (numero > 2 && numero < 4)
+            {
+                conf.Configuracao3();
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
